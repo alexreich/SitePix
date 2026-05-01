1ffb8e5e213aaaa1a1667742990e52b1593dfc45
Alex <alex@example.com>
2026-05-01 08:59:49 -0700

    accidental inclusion

--- SitePix/run_dev.bat
+++
@@ -1,5 +0,0 @@
-set ASPNETCORE_ENVIRONMENT=Development
-set ASPNETCORE_HOSTINGSTARTUPASSEMBLIES=Microsoft.AspNetCore.SpaProxy
-set API_ROOT_URL=https://localhost:57096
-set ASPNETCORE_URLS=https://localhost:5001;http://localhost:5000
-AcornJr.Server.exe
